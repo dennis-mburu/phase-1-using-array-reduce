23a6d178fe3d52eba80a342b11f86fce172ff090
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-06-12 23:48:08 +0300

    commit tree 29080d7cf3d60ebb87c5e0b462c3bc84b8dea0d9

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce(
+    function (accum, elem){
+        return elem+accum
+    },
+0)
+
+// Can also be written in arrow funcyion as:
+// const totalBatteries = batteryBatches.reduce((accum, elem) => (elem + accum),0)
+
+// console.log(totalBatteries)
+
